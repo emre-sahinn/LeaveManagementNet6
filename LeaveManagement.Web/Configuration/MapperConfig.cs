3c547d4cf80f948a50055f548894b4bdbb64cccc
ARXINO <arxtechnologies@gmail.com>
2022-08-09 15:49:24 +0300

    View Model for LeaveType is added.

---
+++ LeaveManagement.Web/Configuration/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LeaveManagement.Web.Data;
+using LeaveManagement.Web.Models;
+
+namespace LeaveManagement.Web.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            //LeaveType'i LeaveTypeVM map ediyoruz ve ReverseMap ile bunun tersi de olabilir diyoruz
+            CreateMap<LeaveType, LeaveTypeVM>().ReverseMap();
+        }
+    }
+}
---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    //view model: database deki bütün satilari gormesine(LeaveType) gerek yok sadece ilgili olanlari(LeaveTypeVM) gorsun
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
